c4a948c8d32a901779ac40b842360d56345bca01
Sinclair Target <sinclairtarget@gmail.com>
2015-12-26 04:28:32 +0800

    um-read.rb now opens um pages using default config.

--- libexec/um-read.rb
+++ libexec/um-read.rb
@@ -1,9 +1,22 @@
-usage = "usage: #{$PROGRAM_NAME} <page name>"
+program_name = File.basename($PROGRAM_NAME)
+usage = "usage: #{program_name} <page name>"
 
-if ARGV.length != 1
-  $stderr.puts usage
+page_name = ARGV.first
+if page_name.nil?
+  $stdout.puts usage
   exit 1
 end
 
-page_name = ARGV[0]
-puts "Page name: #{page_name}"
+# source config
+config = { "pager" => "less", 
+           "pages_directory" => "/Users/sinclairtarget/Dropbox (Personal)/um/pages" }
+topic = "shell"
+
+page_path = "#{config["pages_directory"]}/#{topic}/#{page_name}.txt"
+unless File.exist? page_path
+  msg = %{No um page found for "#{page_name}" under topic "#{topic}."}
+  $stdout.puts msg
+  exit 1
+end
+
+exec(%{#{config["pager"]} "#{page_path}"})
