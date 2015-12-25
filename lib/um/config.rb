4e85f01acd7ec2149a3818010ae2fc598de44e9c
Sinclair Target <sinclairtarget@gmail.com>
2015-12-26 06:24:00 +0800

    Better errors and shellescape.

--- lib/um/config.rb
+++ lib/um/config.rb
@@ -16,14 +16,18 @@ module Config
   def self.parse_config(path)
     config = {}
 
+    parse_error_occurred = false
     File.foreach(path) do |line|
       if line[/(\w+) = ([\w \/\(\)\.]+)/]
-        config[$1] = $2
+        config[$1.downcase] = $2
       elsif line.chomp.length > 0
-        $stderr.puts "Unable to parse configuration file line: '#{line.chomp}'"
+        $stderr.puts "Unable to parse configuration file line #{$.}: " + 
+          "'#{line.chomp}', skipping"
+        parse_error_occurred = true
       end
     end
 
+    $stderr.puts "Your configuration file is #{path}" if parse_error_occurred
     config
   end
 end
--- libexec/um-read.rb
+++ libexec/um-read.rb
@@ -1,22 +1,24 @@
+require 'shellwords'
+require_relative '../lib/um.rb'
+
 program_name = File.basename($PROGRAM_NAME)
 usage = "usage: #{program_name} <page name>"
 
 page_name = ARGV.first
 if page_name.nil?
-  $stdout.puts usage
+  $stderr.puts usage
   exit 1
 end
 
-# source config
-config = { "pager" => "less", 
-           "pages_directory" => "/Users/sinclairtarget/Dropbox (Personal)/um/pages" }
-topic = "shell"
+config = Config.source
+topic = Topic.current
 
 page_path = "#{config["pages_directory"]}/#{topic}/#{page_name}.txt"
 unless File.exist? page_path
   msg = %{No um page found for "#{page_name}" under topic "#{topic}."}
-  $stdout.puts msg
-  exit 1
+  $stderr.puts msg
+  exit 2
 end
 
-exec(%{#{config["pager"]} "#{page_path}"})
+pager = config["pager"].shellescape
+exec(%{#{pager} "#{page_path}"})
