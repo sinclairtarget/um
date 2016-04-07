ab777f290dbf5fc7177812e147c787f651464f4e
Sinclair Target <sinclairtarget@gmail.com>
2016-04-07 16:24:42 -0400

    Commands module clean up.

--- lib/um/commands.rb
+++ lib/um/commands.rb
@@ -4,21 +4,38 @@ module Commands
     "l" => "list",
     "r" => "read",
     "s" => "set",
-    "rm" => "rm",
     "t" => "topic",
     "h" => "help"
   }
 
-  def self.alias(cmd)
+  def self.resolve_alias(cmd)
     ALIASES[cmd]
   end
 
   def self.libexec(cmd)
-    if ALIASES.values.include? cmd
-      dir = File.expand_path("../../libexec", File.dirname(__FILE__))
-      "#{dir}/um-#{cmd}.rb"
+    cmd = resolve_alias(cmd) || cmd 
+    file_path = file_path_for_command(cmd)
+
+    if file_path
+      ARGV.shift
+      run file_path
+    else
+      run file_path_for_command("read")
+    end
+  end
+
+  private
+  def self.file_path_for_command(cmd)
+    dir = File.expand_path("../../libexec", File.dirname(__FILE__))
+    path = "#{dir}/um-#{cmd}.rb"
+    if File.exist?(path)
+      path
     else
       nil
     end
+  end
+
+  def self.run(file_path)
+    exec(%{ruby "#{file_path}" #{ARGV.join(" ")}})
   end
 end
